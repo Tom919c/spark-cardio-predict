eedb8805e1af1fbb32cc07194ba12651ef3105e7
XINGYUAN <2858987538@qq.com>
2026-07-11 10:24:05 +0800

    前端使用者化

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
+import os
+import threading
+import webbrowser
+
 from src import create_app
 
 
 app = create_app()
 
 
+def open_browser():
+    url = "http://127.0.0.1:5000/dashboard"
+    try:
+        os.startfile(url)
+    except AttributeError:
+        webbrowser.open(url)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    threading.Timer(1.5, open_browser).start()
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
